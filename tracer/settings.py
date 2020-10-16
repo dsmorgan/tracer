370d53de073020d127e13513c7e57872d1f51f6c
David Morgan <dsm@nc.rr.com>
2020-10-16 15:05:32 -0400

    fixup settings template to be 1.x instead of 3.x

--- tracer/settings.py
+++ tracer/settings.py
@@ -10,22 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+##from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+##BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zvq7*ze+7%4wvw84mbgo-hwwcehvxi2@2tkn8@n5bbq_y6h59-'
+##SECRET_KEY = 'zvq7*ze+7%4wvw84mbgo-hwwcehvxi2@2tkn8@n5bbq_y6h59-'
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY','zvq7*ze+7%4wvw84mbgo-hwwcehvxi2@2tkn8@n5bbq_y6h59-'
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,11 +80,15 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+##DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': BASE_DIR / 'db.sqlite3',
+##    }
+##}
+from . import database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': database.config()
 }
 
 
@@ -119,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = ['127.0.0.1']
