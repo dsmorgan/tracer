f78d6ac66de0fd0cd7564ee5b15fe878b32e220e
David Morgan <dsm@nc.rr.com>
2020-10-21 10:39:17 -0400

    added nexthost and include full forwarded for header

--- tracerapi/views.py
+++ tracerapi/views.py
@@ -55,7 +55,7 @@ def trace1(request):
         text='[{"trace1end": "OTHER","hopsleft":'+str(hopsleft-1)+'}]' 
     
     jresp=json.loads(text)
-    tt={"hop":hop, "host":nexthost, "scode":scode, "reason":reason, "err":err , "etime":etime, "sip":sip, "cip":cip}
+    tt={"hop":hop, "nexthost":nexthost, "scode":scode, "reason":reason, "err":err , "etime":etime, "sip":sip, "cip":cip}
     jresp.append(tt)
     #jresp.append(tt)
     
@@ -78,7 +78,8 @@ def exec_time(st):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0] + "(" + request.META.get('REMOTE_ADDR') + ")"
+        #ip = x_forwarded_for.split(',')[0] + "(" + request.META.get('REMOTE_ADDR') + ")"
+        ip = x_forwarded_for + "(" + request.META.get('REMOTE_ADDR') + ")"
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
