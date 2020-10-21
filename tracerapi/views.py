2bbd8f6ad668458d0737c4832b1b4dd134232d48
David Morgan <dsm@nc.rr.com>
2020-10-20 23:15:34 -0400

    added xforward and clientip

--- tracerapi/views.py
+++ tracerapi/views.py
@@ -78,7 +78,7 @@ def exec_time(st):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[0] + "(" + request.META.get('REMOTE_ADDR') + ")"
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
